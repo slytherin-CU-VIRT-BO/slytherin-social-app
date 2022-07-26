bfb51ea50f04469db67d07b24d4f53b4c2693c4a
Tommy McG <thomasjmcgovern98@gmail.com>
2022-07-26 16:29:42 -0400

    added sign up/login

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -42,7 +42,31 @@ const resolvers = {
           }
     },
     Mutation: {
-
+        // Create a user. Ideally this is done from the sign up.
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+      
+            return { token, user };
+        },
+        // User an email and password to sign in a user
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+      
+            if (!user) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+      
+            const correctPw = await user.isCorrectPassword(password);
+      
+            if (!correctPw) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+      
+            const token = signToken(user);
+            return { token, user };
+        },
+        
     }
 }
 
