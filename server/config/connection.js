5bba952e9f43cd1b677777677729932db208c064
Jessica Chen <jessicachen28@gmail.com>
2022-07-29 16:21:40 -0400

    Revert Mongo Server URI

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1/slytherin-social-app",
+  process.env.MONGODB_URI || "mongodb://localhost/slytherin-social-app",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
