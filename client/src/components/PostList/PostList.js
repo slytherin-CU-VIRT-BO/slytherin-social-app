506044915bad3bff49a0e6e71f64c88b6011cddc
Ross Thomson <rthomson412@gmail.com>
2022-07-29 20:47:17 -0400

    refix

--- client/src/components/PostList/PostList.js
+++ client/src/components/PostList/PostList.js
@@ -1,19 +1,16 @@
 import React from 'react';
 // import './PostList.css';
-const PostList = ({ posts }) => {
+export default function PostList({ posts }) {
   if (!posts.length) {
-      return <h3>No Posts Yet</h3>;
-    }
+    return <h3 className="title">No Posts Yet</h3>;
+  }
+
   return (
     <div>
 
       <div className="dash">
         <div className="stats">
           <h1 className="title">Posts</h1>
-        </div>
-      </div>
-      <div className="dash">
-        <div className="stats">
           {posts &&
             posts.map(post => (
               <div key={post._id} className="title">
@@ -28,4 +25,3 @@ const PostList = ({ posts }) => {
     </div>
   );
 }
-export default PostList;
