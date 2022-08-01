b27c90ba40bbb663e750aa1f6348ba51b80b1811
SplitRabbit <jue.kun.liu@gmail.com>
2022-08-01 14:16:09 -0400

    added in profile edit changes

--- client/src/components/ProfilePage/ProfilePage.js
+++ client/src/components/ProfilePage/ProfilePage.js
@@ -4,6 +4,7 @@ import ProfilePosts from '../ProfilePosts/ProfilePosts'
 import FriendsList from '../FriendsList/FriendsList'
 import { useQuery } from "@apollo/client";
 import { GET_ME } from '../../utils/queries';
+import { Link } from "react-router-dom";
 
 const ProfilePage = () => {
   const { loading, error, data } = useQuery(GET_ME)
@@ -18,6 +19,10 @@ const ProfilePage = () => {
       <div className="profile-page">
         <h2 className='profile-name'>Slytherin fellow {data.firstName} {data.lastName}</h2>
         {/* {userParam && (<button className="addfriend" onClick={handleClick}>Add Friend</button>)} */}
+        {/* Insert link to edit profile component here */}
+        <p>
+          Edit Profile<Link to="/editprofile">Sign In.</Link>
+        </p>
       </div>
 
       <main className="profile-container">
