29d214a78054fbaa07b5f5992a6775091d45eda7
Ross Thomson <rthomson412@gmail.com>
2022-08-01 17:07:08 -0400

    submit

--- client/src/components/NewPost/NewPost.js
+++ client/src/components/NewPost/NewPost.js
@@ -4,7 +4,7 @@ import './NewPost.css';
 const NewPost = ({ onSubmit }) => {
 return (
 <article className="newpost gradient-border">
-  <form onSubmit={submitHandler}>
+  <form onSubmit={onSubmit}>
     <textarea placeholder="What are you slythering about?"/>
     <div className="btns">
       <label htmlFor="PostText">
