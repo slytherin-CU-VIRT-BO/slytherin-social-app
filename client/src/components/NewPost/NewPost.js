04e055ef3f3636195cd65c26be45a51f56c19ec0
SplitRabbit <jue.kun.liu@gmail.com>
2022-08-03 18:30:12 -0400

    fixed posts

--- client/src/components/NewPost/NewPost.js
+++ client/src/components/NewPost/NewPost.js
@@ -1,13 +1,48 @@
-import { isConstValueNode } from 'graphql';
-import React from 'react';
+import { useMutation } from "@apollo/client";
+import React, { useState } from "react";
 import './NewPost.css';
-import { FaPhotoVideo, FaRegPaperPlane} from 'react-icons/fa'
+import { FaPhotoVideo, FaRegPaperPlane} from 'react-icons/fa';
+import { ADD_POST } from "../../utils/mutations";
 
-const NewPost = ({ onSubmit }) => {
+const NewPost = () => {
+    const [formState, setFormState] = useState({
+      postText: ""
+    });
+    
+    const [errorMessage, setErrorMessage] = useState("");
+
+    const [addPost] = useMutation(ADD_POST);
+   
+    const formSubmit = async (event) => {
+      event.preventDefault();
+      event.stopPropagation();
+
+      addPost({
+          variables: {
+            postText: formState.postText
+          },
+        });
+      };
+
+      const handleChange = (event) => {
+        if (!event.target.value.length) {
+          setErrorMessage(`${event.target.name} is required.`);
+        } else {
+          setErrorMessage("");
+    
+          const { name, value } = event.target;
+          setFormState({ ...formState, [name]: value });
+        }
+      };
+
 return (
 <article className="newpost gradient-border">
-  <form onSubmit={onSubmit}>
-    <textarea placeholder="What are you slythering about?"/>
+  <form onSubmit={formSubmit}>
+    <textarea 
+        placeholder="What are you slythering about?"
+        name='postText'
+        onChange={handleChange}
+    />
     <div className="btns">
       <label htmlFor="PostText">
       </label>
@@ -16,6 +51,11 @@ return (
         < FaPhotoVideo />
       </button>
     </div>
+    {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
   </form>
 </article>
 );
