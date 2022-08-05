28b3e91e0338b82b1db2fe61f5af4c223f51596f
SplitRabbit <jue.kun.liu@gmail.com>
2022-08-05 15:20:29 -0400

    changes to header functionality

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -2,6 +2,8 @@ import { FaBars } from "react-icons/fa";
 import "./Header.css";
 import React from "react";
 import { Link } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { GET_ME } from "../../utils/queries";
 import Auth from "../../utils/auth";
 import {
   Nav,
@@ -12,58 +14,80 @@ import {
   NavBtnLink,
 } from "./HeaderElements";
 
-export default function Header() {
+const Header = () => {
+  const { loading, error, data } = useQuery(GET_ME,{skip:!Auth.loggedIn()});
+  
   const logout = (event) => {
     event.preventDefault();
     Auth.logout();
   };
 
-  return (
-    <header className="header">
-      {Auth.loggedIn() ? (
-        <>
-          <Nav>
-            <NavLink to="/">
-              <h1>SlytherinSocial</h1>
-            </NavLink>
-            <Bars />
-            <NavLink to="/" activestyle="true">
-              Home
-            </NavLink>
-            <NavMenu>
-              <NavLink to="/newpost" activestyle="true">
-                Post
-              </NavLink>
-              <NavLink to="/profile" activestyle="true">
-                Profile
-              </NavLink>
-              <NavBtnLink to="/" onClick={logout} activestyle="false">
-                Logout
-              </NavBtnLink>
-            </NavMenu>
-          </Nav>
-        </>
-      ) : (
-        <>
-          <Nav>
-            <NavLink to="/">
-              <h1>SlytherinSocial</h1>
-            </NavLink>
-            <Bars />
-            <NavMenu>
-              <NavLink to="/" activestyle="true">
-                Home
-              </NavLink>
-              <NavLink to="/signup" activestyle="true">
-                Signup
-              </NavLink>
-              <NavLink to="/login" activestyle="true">
-                Login
-              </NavLink>
-            </NavMenu>
-          </Nav>
-        </>
-      )}
-    </header>
-  );
-}
+  const renderPage = () => {
+    if (loading) {
+      return <p>Loading...</p>;
+    } else if (error) {
+      return <p>Something Bad Happened</p>;
+    } else {
+      return (
+        <header className="header">
+          {Auth.loggedIn() ? (
+            <>
+              <Nav>
+                <NavLink to="/">
+                  <h1>SlytherinSocial</h1>
+                </NavLink>
+                <Bars />
+                <NavLink to="/" activestyle="true">
+                  Home
+                </NavLink>
+                <NavMenu>
+                  <NavLink to="/newpost" activestyle="true">
+                    Post
+                  </NavLink>
+                  <NavLink 
+                    to={{
+                      pathname:"/profile",
+                      profileProps: {
+                        username: data.me.username
+                      }
+                    }}
+                    activestyle="true">
+                    Profile
+                  </NavLink>
+                  <NavBtnLink to="/" onClick={logout} activestyle="false">
+                    Logout
+                  </NavBtnLink>
+                </NavMenu>
+              </Nav>
+            </>
+          ) : (
+            <>
+              <Nav>
+                <NavLink to="/">
+                  <h1>SlytherinSocial</h1>
+                </NavLink>
+                <Bars />
+                <NavMenu>
+                  <NavLink to="/" activestyle="true">
+                    Home
+                  </NavLink>
+                  <NavLink to="/signup" activestyle="true">
+                    Signup
+                  </NavLink>
+                  <NavLink to="/login" activestyle="true">
+                    Login
+                  </NavLink>
+                </NavMenu>
+              </Nav>
+            </>
+          )}
+        </header>
+      );
+    }
+  };
+
+  return <div>{renderPage()}</div>;
+};
+
+export default Header;
+
--- client/src/components/ProfilePage/ProfilePage.js
+++ client/src/components/ProfilePage/ProfilePage.js
@@ -5,41 +5,46 @@ import ProfilePosts from "../ProfilePosts/ProfilePosts";
 import FriendsList from "../FriendsList/FriendsList";
 import NewPost from "../NewPost/NewPost";
 import { useQuery } from "@apollo/client";
-import { GET_ME } from "../../utils/queries";
+import { GET_USER } from "../../utils/queries";
 import { Link } from "react-router-dom";
 
-const ProfilePage = () => {
-  const { loading, error, data } = useQuery(GET_ME);
+const ProfilePage = (props) => {
+  const { loading, error, data } = useQuery(GET_USER, {
+    variables: {
+      username: props.username
+    }
+  });
 
   const renderPage = () => {
     if (loading) {
-      return <p>Loading...</p>;
-    } else if (error) {
-      return <p>Something Bad Happened</p>;
-    } else {
+      return <div>Loading...</div>;
+    }
+    if (error) {
+      console.error(error);
+      return <div>Error!</div>;
+    }
       return (
         <div>
           <h2 className="profile-name">
-            {data.me.firstName} {data.me.lastName}
+            {data.user.firstName} {data.user.lastName}
           </h2>
 
           <section className="dash">
             <div className="dashleft">
-              <ProfileCard data={data.me} />
+              <ProfileCard data={data.user} />
             </div>
 
             <main className="dashmiddle">
               <NewPost />
-              {/* <ProfilePosts posts={data.me.posts} /> */}
+              {/* <ProfilePosts posts={data.user.posts} /> */}
             </main>
 
             <aside className="dashright userlist">
-              <FriendsList friends={data.me.friends} />
+              <FriendsList friends={data.user.friends} />
             </aside>
           </section>
         </div>
       );
-    }
   };
 
   return <div>{renderPage()}</div>;
