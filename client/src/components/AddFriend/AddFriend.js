ea5785ad39c19e6c958b98d33b87da7f9dc9eae3
SplitRabbit <jue.kun.liu@gmail.com>
2022-08-05 11:20:14 -0400

    button additions

--- client/src/components/AddFriend/AddFriend.js
+++ client/src/components/AddFriend/AddFriend.js
@@ -3,14 +3,43 @@ import {FaUserPlus,FaUserMinus,GiCancel} from "react-icons/fa";
 import React from "react";
 import "./AddFriend.css";
 
-const AddFriend = ({ data }) => {
-  
+function CancelFriendRequest(props) {
   return (
-    <button >
-        Add Friend
-        <FaUserPlus />
+    <button onClick={props.onClick}>
+      Cancel Friend Request
+      <GiCancel />
     </button>
   );
+};
+
+function RemoveFriend(props) {
+  return (
+    <button onClick={props.onClick}>
+      Remove Friend
+      <FaUserMinus />
+    </button>
+  );
+};
+
+function SendFriendRequest(props) {
+  return (
+    <button onClick={props.onClick}>
+      Add Friend
+      <FaUserPlus />
+    </button>
+  );
+};
+
+const AddFriend = ({ data }) => {
+    if(potato==data.potato) {
+
+    } else if(potato!=data.potato) {
+      return
+
+    } else {
+      return
+
+    };
 };
 
 export default AddFriend;
