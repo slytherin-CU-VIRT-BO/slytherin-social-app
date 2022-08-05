b7970be200584e9bd57f0575d3ffd49fee8a1b17
SplitRabbit <jue.kun.liu@gmail.com>
2022-08-05 16:25:09 -0400

    testuser for profilepage in

--- client/src/components/ProfilePage/ProfilePage.js
+++ client/src/components/ProfilePage/ProfilePage.js
@@ -11,27 +11,57 @@ import { Link } from "react-router-dom";
 const ProfilePage = (props) => {
   const { loading, error, data } = useQuery(GET_USER, {
     variables: {
-      username: props.username
+      username: "potato"
     }
   });
 
+  const testUser = {
+  
+      "user": {
+        "username": "potato",
+        "firstName": null,
+        "lastName": null,
+        "email": "potato@gmail.com",
+        "posts": [
+          {
+            "postText": "potato",
+            "createdAt": "Aug 3rd, 2022 at 6:28 pm",
+            "comments": []
+          },
+          {
+            "postText": "potato",
+            "createdAt": "Aug 3rd, 2022 at 6:28 pm",
+            "comments": []
+          },
+          {
+            "postText": "potato",
+            "createdAt": "Aug 3rd, 2022 at 6:28 pm",
+            "comments": []
+          }
+        ],
+        "friends": [],
+        "friendRequests": []
+
+    }
+  };
+
   const renderPage = () => {
-    if (loading) {
-      return <div>Loading...</div>;
-    }
-    if (error) {
-      console.error(error);
-      return <div>Error!</div>;
-    }
+    // if (loading) {
+    //   return <div>Loading...</div>;
+    // }
+    // if (error) {
+    //   console.error(error);
+    //   return <div>Error!</div>;
+    // }
       return (
         <div>
           <h2 className="profile-name">
-            {data.user.firstName} {data.user.lastName}
+            {testUser.user.firstName} {testUser.user.lastName}
           </h2>
 
           <section className="dash">
             <div className="dashleft">
-              <ProfileCard data={data.user} />
+              <ProfileCard data={testUser.user} />
             </div>
 
             <main className="dashmiddle">
@@ -40,7 +70,7 @@ const ProfilePage = (props) => {
             </main>
 
             <aside className="dashright userlist">
-              <FriendsList friends={data.user.friends} />
+              <FriendsList friends={testUser.user.friends} />
             </aside>
           </section>
         </div>
@@ -51,3 +81,4 @@ const ProfilePage = (props) => {
 };
 
 export default ProfilePage;
+
